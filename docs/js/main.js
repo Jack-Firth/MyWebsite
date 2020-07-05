51f7967a4f100f9ccc8d880a1ce1c2ee836f7221
Jack-Firth <jdavidfirth@gmail.com>
2020-07-05 12:00:54 +0100

    Restructuring to work with github pages

---
+++ docs/js/main.js
@@ -0,0 +1,23 @@
+function toggleDropDown() {
+  let navbarToggle = document.getElementById("navbar-toggle");
+  if (navbarToggle.className === "nav-menu-list") {
+    navbarToggle.className += "-responsive";
+  } else {
+    navbarToggle.className = "nav-menu-list";
+  }
+}
+
+function stickyNavBar() {
+  let navigationBar = document.getElementById("navigation-bar");
+  let stickyBar = navigationBar.offsetTop;
+  if (window.pageYOffset >= stickyBar) {
+    navigationBar.classList.add("-sticky");
+  } else {
+    navigationBar.classList.remove("-sticky");
+  }
+  console.log("sticky");
+}
+
+window.onscroll = function () {
+  stickyNavBar();
+};
