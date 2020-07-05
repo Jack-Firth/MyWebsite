3d7e4f7cfe7086e950f080d48b39b37519f44754
Jack-Firth <jdavidfirth@gmail.com>
2020-07-05 16:57:25 +0100

    php troubleshooting

--- docs/contact-form-handler.php
+++ docs/contact-form-handler.php
@@ -4,13 +4,13 @@ error_reporting(E_ALL)
 ini_set('Error Reporting', E_ALL)
 
 $name = $_POST['contactForm-name'];
-$subject = "New Website Contact Submission";
+$subject = 'New Website Contact Submission';
 $mailFrom = $_POST['contactForm-email'];
 $message = $_POST['contactForm-message'];
 
-$mailTo = "me@jackfirth.com";
-$headers = "From: ".$mailFrom;
-$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+$mailTo = 'me@jackfirth.com';
+$headers = 'From: $mailFrom';
+$txt = 'You have received an e-mail from .$name. .\n\n .$message';
 
 if ($_POST['submit']) {
     if (mail($mailTo, $subject, $txt, $headers)) {
