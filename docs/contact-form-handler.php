3a888771c237aa2fcc648fdf13b7142cd3b0b633
Jack-Firth <jdavidfirth@gmail.com>
2020-07-05 12:24:51 +0100

    Working with php contact form

--- docs/contact-form-handler.php
+++ docs/contact-form-handler.php
@@ -3,15 +3,15 @@
 if (isset($_POST['submit']))  {
     $name = $_POST['contact-name'];
     $subject = "New Website Contact Submission";
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+    $mailFrom = $_POST['contact-email'];
+    $message = $_POST['contact-message'];
     
     $mailTo = "me@jackfirth.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsent");
+    header("Location: index.html?mailsent");
 }  
 
 ?>
