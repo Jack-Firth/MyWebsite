38e805acff82ef394cbc4ee8ebe2dd2a8afdf1be
Jack-Firth <jdavidfirth@gmail.com>
2020-07-05 12:09:52 +0100

    Working with php contact form

--- docs/contact-form-handler.php
+++ docs/contact-form-handler.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit']))  {
     
     $mailTo = "me@jackfirth.com";
     $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
     mail($mailTo, $subject, $txt, $headers);
     header("Location: index.html?mailsend");
